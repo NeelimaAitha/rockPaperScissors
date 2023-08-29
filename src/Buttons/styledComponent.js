8e88888b9cc370b982ce7cb35c3bf925fea525d1
neelima <aithaneelima@gmai.com>
2023-08-29 13:05:36 +0000

    first commit

---
+++ src/Buttons/styledComponent.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const ButtonLiContainer = styled.li`
+  list-style: none;
+`
+
+export const ButtonImage = styled.button``
+
+export const ImageItem = styled.img``
---
+++ src/Game/styledComponent.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div``
+
+export const ScoreContainer = styled.div``
+
+export const ItemsContainer = styled.div``
+
+export const Heading = styled.h1``
+
+export const ScoreCardContainer = styled.div``
+
+export const ParagraphScore = styled.p``
+
+export const ScoreSpan = styled.p``
+
+export const ItemsImagesContainer = styled.ul``
+
+export const PlayAgainButton = styled.button``
+
+export const PopUpContainer = styled.div``
+
+export const PopUpButton = styled(PlayAgainButton)``
+
+export const RulesImageContainer = styled.div``
+
+export const RulesImage = styled.img``
+
+export const CloseLineButton = styled.button``
+
+export const CloseLineContainer = styled.div``
